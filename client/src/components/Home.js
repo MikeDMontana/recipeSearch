673abded768386d8b09e85e57fc1cf9e6581c754
Mike Dreiling <mdarts@gmail.com>
2017-08-27 14:14:26 -0600

    loading gif in place

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {TimelineMax} from 'gsap';
+import GSAP from 'react-gsap-enhancer';
 
 class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      searchResults: []
+      searchResults: [],
+      gifVisibility: "notVisible"
     }
   }
 
@@ -18,19 +21,24 @@ class Home extends Component {
   }
 
   searchSubmitHandler(event) {
+    this.isLoading();
+
     axios.get("/api/recipes/" + this.state.recipeSearch)
       .then((response) => {
         const searchResults = response.data.recipes;
         console.log(searchResults);
         this.setState({
-          searchResults: searchResults
+          searchResults: searchResults,
+          gifVisibility: "notVisible"
         });
       });
     event.preventDefault();
   }
 
-  hoverHandler(event) {
-    console.log(event.target.value);
+  isLoading() {
+      this.setState({
+        gifVisibility: "visibile"
+      });
   }
 
 
@@ -38,12 +46,16 @@ class Home extends Component {
     return (
       <div className="pageWrapper">
         <div className="searchSide">
-          <img src="/img/logo.png" />
+          <img src="/img/logo.png" alt="Foodies Find It A React Visual Experiment By Mike Dreiling"/>
           <form className="recipeSearchForm" onSubmit={this.searchSubmitHandler.bind(this)}>
             <input className="recipeSearchInput" type="text" onChange={this.recipeChangeHandler.bind(this)} placeholder="SEARCH FOR RECIPES" />
             <input className="recipeSearchSubmit" type="submit" value="submit" />
           </form>
+          <div className="aboutContainer">
+            <p className="aboutProjectText">A visual exploration of the <a href="#">Food2Fork API</a><br />from <a href="http://www.creativedesignsbymike.com" target="_blank">Mike Dreiling</a></p>
+          </div>
         </div>
+        <img src="img/ajax-loader.gif" className={this.state.gifVisibility} />
         <ul className="gridSide">
             {this.state.searchResults.map((recipe) =>
               <div className="recipeContainer">
